daeb034875b222d40d4046d51fd72ae97333eff4
tonystdeng <tony.st.deng@gmail.com>
2025-03-09 17:23:18 -0400

    fix api.py

--- api.py
+++ api.py
@@ -11,7 +11,7 @@ app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("./dist/index.html")
+    return render_template("index.html")
 
 
 @app.route("/api/c2m",methods=["GET"])
