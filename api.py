639782c178c36520fd5c2198d625db0b20a309d7
tonystdeng <tony.st.deng@gmail.com>
2025-03-09 16:24:42 -0400

    complete api code

--- api.py
+++ api.py
@@ -1,10 +1,139 @@
-from flask import Flask
+from flask import Flask,jsonify,render_template,request
+import json
+
+with open("morse-code.json") as file:
+    m2cDict=json.load(file)
+
+c2mDict={value:key for key,value in m2cDict.items()}
+
 
 app=Flask(__name__)
 
 @app.route("/")
-def home():
-    return "<h1>Flask API follow youtube tutorial</h1>"
+def index():
+    return "<h1>An API that translates morse code!</h1>"
 
-if __name__=="__main__":
-    app.run(debug=True)
+
+@app.route("/api/m2c",methods=["GET"])
+def m2c():
+    return jsonify(m2cDict)
+
+@app.route("/api/m2c/alpha",methods=["GET"])
+def m2cAlpha():
+    alpha={}
+    for key in m2cDict.keys():
+        if key.isalpha():
+            alpha[key]=m2cDict[key]
+    return jsonify(alpha)
+
+@app.route("/api/m2c/numeric",methods=["GET"])
+def m2cNumeric():
+    numeric={}
+    for key in m2cDict.keys():
+        if key.isnumeric():
+            numeric[key]=m2cDict[key]
+    return jsonify(numeric)
+
+@app.route("/api/m2c/symbol",methods=["GET"])
+def m2cSymbol():
+    symbol={}
+    for key in m2cDict.keys():
+        if not(key.isnumeric() or key.isalpha()):
+            symbol[key]=m2cDict[key]
+    return jsonify(symbol)
+
+@app.route("/api/m2c/char",methods=["GET"])
+def m2cChar():
+    char=request.args.get('key')
+    if not char:
+        return jsonify({"error": "'key' parameter is required"}), 400
+    if not char in m2cDict.keys():
+        return jsonify({"error": "'key' parameter is invalid: value not found in database"}), 400
+    return jsonify(m2cDict[char])
+
+
+
+
+@app.route("/api/c2m",methods=["GET"])
+def c2m():
+    return jsonify(c2mDict)
+
+@app.route("/api/c2m/alpha",methods=["GET"])
+def c2mAlpha():
+    alpha={}
+    for key in m2cDict.keys():
+        if key.isalpha():
+            alpha[m2cDict[key]]=key
+    return jsonify(alpha)
+
+@app.route("/api/c2m/numeric",methods=["GET"])
+def c2mNumeric():
+    numeric={}
+    for key in m2cDict.keys():
+        if key.isnumeric():
+            numeric[m2cDict[key]]=key
+    return jsonify(numeric)
+
+@app.route("/api/c2m/symbol",methods=["GET"])
+def c2mSymbol():
+    symbol={}
+    for key in m2cDict.keys():
+        if not(key.isnumeric() or key.isalpha()):
+            symbol[m2cDict[key]]=key
+    return jsonify(symbol)
+
+@app.route("/api/c2m/char",methods=["GET"])
+def c2mChar():
+    char=request.args.get('key')
+    if not char:
+        return jsonify({"error": "'key' parameter is required"}), 400
+    if not char in c2mDict.keys():
+        return jsonify({"error": "'key' parameter is invalid: value not found in database"}), 400
+    return jsonify(c2mDict[char])
+
+
+@app.route("/api/m2c",methods=["POST"])
+def m2cPost():
+    global m2cDict, c2mDict
+    errorformating=jsonify({"error":"bad formating, good format example: {char0: morse_code0, char1: morse_cose1, ...}"}),400
+    new=request.json
+    # encountering errors
+    if type(new)!=dict:
+        return errorformating
+    for key,value in new.items():
+        if not (type(key)==str and type(value)==str):
+            return errorformating
+        if len(key)!=1:
+            return errorformating
+        if key in m2cDict.keys() or value in m2cDict.values():
+            return jsonify({"error": "data conflicting with what is already existing in database"}),400
+    # actually adding to database
+    m2cDict.update(new)
+    c2mDict={value:key for key,value in m2cDict.items()}
+    with open("morse-code.json",'w') as file:
+        json.dump(m2cDict,file)
+    return jsonify({"success": f"""the following data is successfully added to the database: {new}, thank you for helping us"""}),202
+
+@app.route("/api/c2m",methods=["POST"])
+def c2mPost():
+    global m2cDict, c2mDict
+    errorformating=jsonify({"error":"bad formating, good format example: {morse_code0: char0, morse_cose1: char1, ...}"}),400
+    new=request.json
+    # encountering errors
+    if type(new)!=dict:
+        return errorformating
+    for key,value in new.items():
+        if not (type(key)==str and type(value)==str):
+            return errorformating
+        if len(value)!=1:
+            return errorformating
+        if key in c2mDict.keys() or value in c2mDict.values():
+            return jsonify({"error": "data conflicting with what is already existing in database"}),400
+    # actually adding to database
+    c2mDict.update(new)
+    m2cDict={value:key for key,value in c2mDict.items()}
+    with open("morse-code.json",'w') as file:
+        json.dump(m2cDict,file)
+    return jsonify({"success": f"""the following data is successfully added to the database: {new}, thank you for helping us"""}),202
+
+if __name__=="__main__": app.run()
